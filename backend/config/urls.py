ee48b180db71079e6b9bb0b1d7692cc1869bde80
fenrir-mack <10436455@mackenzista.com.br>
2026-04-12 14:41:51 -0300

    corrigindo nomes do django

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Projeto project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
